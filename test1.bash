0063d32f52f25aac8a8a606c1d5d1337450b5d07
hitokoto takumi <takumihekoheko>
2023-11-27 15:27:47 +0900

    123

--- test1.bash
+++ test1.bash
@@ -11,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
