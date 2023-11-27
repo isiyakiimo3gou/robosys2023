d8bba0a54ecea41695e3296d50976c7c2f4ee70f
hitokoto takumi <takumihekoheko>
2023-11-28 04:10:41 +0900

    a

--- test.bash
+++ test.bash
@@ -3,12 +3,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-      echo NG at Line $1
-      res=1
+	echo NG at Line $1
+	res=1
 }
-　
+
 res=0
-　
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
