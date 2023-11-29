6ed4b4cbd091394a87b5dcd163ac01f3c91052d4
hitokoto takumi <takumihekoheko>
2023-11-29 11:54:51 +0900

    a

--- test.bash
+++ test.bash
@@ -6,13 +6,20 @@ ng () {
         echo NG at Line $1
         res=1
 }
-　
+
 res=0
-　
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
-　
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
-
